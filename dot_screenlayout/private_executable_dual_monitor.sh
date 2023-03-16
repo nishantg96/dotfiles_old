ec1ce498edaace1f9f767973c7828ad4abb320e5
Nishant Gajjar <n.gajjar@reply.de>
2023-03-16 23:37:35 +0100

    Shit load of updated! :D

---
+++ dot_screenlayout/private_executable_dual_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1200 --pos 0x1440 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal
