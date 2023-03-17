dd4bf62f95620e5bd23b282b71c938e3091169d5
Nishant Gajjar <n.gajjar@reply.de>
2023-03-17 01:13:23 +0100

    @#$@ I told you I am not adding any more commit msgs.

--- dot_screenlayout/private_executable_dual_monitor.sh
+++ dot_screenlayout/private_executable_dual_monitor.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1200 --pos 0x1440 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DVI-I-5-4 --off --output DVI-I-4-3 --off --output DVI-I-3-2 --off --output DVI-I-2-1 --primary --mode 3440x1440 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --above DVI-I-2-1
+xrandr --output eDP-1 --mode 1920x1200 --rate 60 --below DVI-I-2-1
